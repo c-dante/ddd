f551a457826b7ab9147865cbb9304d4f94194b53
dante <c.dante.federici@gmail.com>
2017-11-12 14:55:39 -0700

    More planets!

--- src/scene/weather.js
+++ src/scene/weather.js
@@ -25,55 +25,72 @@ const ellipsePath = (vec, t, a = 1, b = 1) => vec.set(
 	b * Math.sin(t)
 );
 
+// WOMP.
+const makeOrbitPlanet = (
+	size = 5,
+	material = materials.wireframe(),
+	a = 1000,
+	b = 1000,
+) => ({ mesh: planet(size, material), a, b });
+
 export default (container) => {
 	const c = util.defaultCamera(container);
 	const s = util.defaultScene(container, c.camera);
 
-	const zion = planet(10, materials.wireframe());
-	const sol = planet(100, materials.wireframe(0xFF0000));
+	// Center
+	const sol = makeOrbitPlanet(100, materials.wireframe(0xFF0000));
 
-	c.camera.position.set(0, 3000, 0);
-	c.camera.lookAt(zion.position);
+	// Satellites
+	const zion = makeOrbitPlanet(10, materials.wireframe());
+	const oz = makeOrbitPlanet(6, materials.wireframe(0x33FF33), 1010, 800);
+	const graftis = makeOrbitPlanet(10, materials.wireframe(0x0000FF), 2000, 2000);
+	const nova = makeOrbitPlanet(25, materials.wireframe(0xFF6600), 1500, 800);
+	
+	// Save our planets
+	const planets = [zion, oz, graftis, nova];
+	
+	// Add them
+	s.scene.add(sol.mesh);
+	planets.forEach(planet => s.scene.add(planet.mesh));
 
-	// Fly controls on camera
+
+	// Look at stuff
+	c.camera.position.set(0, 3500, 0);
+	c.camera.lookAt(sol.mesh.position);
 	c.render = orbitControls(c.camera, {
 		run: 0.5,
 		turn: 0.03,
 	});
 
-	// c.camera.lookAt;
-	// c.camera.translateOnAxis();
-	s.scene.add(zion);
-	s.scene.add(sol);
-
+	
+	// Make a person on zion
 	const p = new Mesh(new SphereGeometry(0.1), materials.wireframe(0x00FF00));
-	zion.add(p);
-	// Pick a point on zion
-	const pt = fp.sample(zion.geometry.vertices).clone();
+	zion.mesh.add(p);
+
+	// Standing at a random point
+	const pt = fp.sample(zion.mesh.geometry.vertices).clone();
 	p.position.set(pt.x, pt.y, pt.z);
 
 	// And our render pipeline
 	const render = (clock) => {
-		zion.rotateOnAxis(spin, rotSpeed);
+		// Rotate zion @todo: other planets
+		zion.mesh.rotateOnAxis(spin, rotSpeed);
 
-		// Run sol's orbit
-		ellipsePath(
-			zion.position,
-			clock.getElapsedTime(),
-			1000,
-			750
-		);
+		// Run planet orbits
+		planets.forEach(planet => {
+			ellipsePath(
+				planet.mesh.position,
+				clock.getElapsedTime(),
+				planet.a,
+				planet.b
+			);
+		})
 	};
 
-	const solUi = oui(sol.position, {
-		title: 'sol.position',
-	});
-
 	return {
-		solUi,
-		// objects
+		// meshs
 		c, s,
 		// pass on the render pipeline
-		...util.pipeline([c, s, { render }, solUi]),
+		...util.pipeline([c, s, { render }]),
 	};
 };
